4c88e6b523d7f26cd46ab6518af2163e5cd6ce48
Devin Proothi <dproothi@student.ubc.ca>
2025-07-22 20:26:11 +0530

    created main chat screen with bottom navigation bar

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,20 +1,9 @@
-// src/navigation/AppNavigator.tsx
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignInScreen from '../screens/Auth/SignInScreen';
-import SignUpScreen from '../screens/Auth/SignUpScreen';
-import HomeScreen from '../screens/HomeScreen';
-import type { RootParamList } from './types';
-
-const Stack = createNativeStackNavigator<RootParamList>();
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+import AuthNavigator from "./AuthNavigator";
+import MainTabNavigator from "./MainTabNavigator";
 
 export default function AppNavigator() {
-  return (
-    <Stack.Navigator
-      // hide header for *all* screens in this stack
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="SignIn" component={SignInScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  );
+  const { token } = useAuth();
+  return token == null ? <AuthNavigator /> : <MainTabNavigator />;
 }
